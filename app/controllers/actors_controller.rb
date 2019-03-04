f6972802618b14dc0bc4e0c64ed5e5b86722e115
Carlos Tanseco <cmtanseco@gmail.com>
2019-03-04 09:49:54 -0600

    Created an actors controller and added actor navigations.

---
+++ app/controllers/actors_controller.rb
@@ -0,0 +1,9 @@
+class ActorsController < ApplicationController
+  def index
+    @actors = Actor.order(:name)
+  end
+
+  def show
+    @actor = Actor.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  # get 'actors/index'
+  # get 'actors/show'
   # get 'movies/index'
   # get 'movies/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :movies, only: [:index, :show]
+  resources :actors, only: [:index, :show]
 
-  
+
   root to: 'movies#index'
 end
